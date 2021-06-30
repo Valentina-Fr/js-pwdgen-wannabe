bfc5fe522a860d68686a4e49872e2868f266f717
Valentina-Fr <v_frasca@virgilio.it>
2021-06-30 16:07:15 +0200

    Declare and assign var

--- js/script.js
+++ js/script.js
@@ -10,3 +10,18 @@ console.log('JS OK');
 6-Generare password in html
  */
 
+// Dichiarazioni e assegnazioni variabili
+var firstName = prompt('Qual è il tuo nome?');
+console.log('nome utente: ', firstName);
+
+var lastName = prompt('Qual è il tuo cognome?');
+console.log('cognome utente: ', lastName);
+
+var favoriteColor = prompt('Qual è il tuo color preferito?');
+console.log('colore utente: ', favoriteColor);
+
+var message = 'La tua password è: ';
+console.log('messaggio: ', message);
+
+var passwordPlaceholder = document.getElementById('password');
+console.log('elemento html: ', passwordPlaceholder);
