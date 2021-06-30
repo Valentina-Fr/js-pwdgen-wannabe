cb001f0556cf22c5bb8b7db256da9b39177e3ab4
Valentina-Fr <v_frasca@virgilio.it>
2021-06-30 15:54:01 +0200

    Check js and comment steps

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Verifica js
+console.log('JS OK');
+
+/* Step da affrontare:
+1-Dichiarare variabile per ottenere nome utente
+2-Dichiarare variabile per ottenere cognome utente
+3-Dichiarare variabile per ottenere colore preferito
+4-Dichiarare variabile messaggio da visualizzare
+5-Dichiarare variabile elemento html da modificare
+6-Generare password in html
+ */
+
